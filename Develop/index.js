d86f074f4199c43b34f96bab7241b5959bc211c6
Gwenn RT <gwenncreative@2020-MBP.attlocal.net>
2024-01-28 16:48:37 -0600

    added in required and questions to index.js

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,56 @@
-// TODO: Include packages needed for this application
+// Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Enter a descrition for your project.'
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Enter installation instructions for your project.'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Enter usage information for your project.'
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'Enter contribution guidelines for your project.'
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Enter test instructions for your project.'
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Select the license you will be using for your project.',
+        choices: ['MIT','Apache','GPL']
+    },
+    {
+        type: 'input',
+        name: 'username',
+        message: 'Enter your GitHub username.'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Enter your email.'
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
